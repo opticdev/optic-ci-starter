088762789de11ca5c8f7c6464bf300685a8126c3
lou <lou@useoptic.com>
2022-01-11 23:20:50 -0500

    implement default breaking changes rules

--- src/ruleset/index.ts
+++ src/ruleset/index.ts
@@ -1,8 +1,6 @@
 import {ApiCheckDslContext, ApiCheckService} from "@useoptic/api-checks";
-import {rules} from "./rules";
 
 export function customRuleset() {
   const service = new ApiCheckService<ApiCheckDslContext>();
-  service.useRules(rules)
   return service;
 }
