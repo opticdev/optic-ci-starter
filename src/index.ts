7ee98a315a8b474a1bba6eefa358fa41e064d31f
Aidan Cunniffe <acunniffe@gmail.com>
2022-01-16 09:57:29 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,13 @@ const config: StandardApiChecks = {
 
 const cli = makeCiCliWithNamedRules(packageJson.name, {
   default: makeApiChecksForStandards(config)
-});
+}, {
+  opticToken: process.env.OPTIC_TOKEN || '123',
+  gitProvider: {
+    token: process.env.GITHUB_TOKEN || '123',
+    provider: 'github',
+  },
+  ciProvider: 'github',
+} );
 
 cli.parse(process.argv);
